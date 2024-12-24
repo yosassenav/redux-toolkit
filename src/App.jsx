96009167d033b32e6a001d863e49f8c63e9f9f1d
yosassenav <mromerovanessa.soy@gmail.com>
2024-12-23 23:56:18 -0500

    getting started with 'react-redux' hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './slices/counter';
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {counter} = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={()=>{dispatch(increment())}}>
+          count is {counter}
         </button>
       </div>
     </>
