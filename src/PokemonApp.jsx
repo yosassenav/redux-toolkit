fc4ad12d18b7ccd3e567f78217d972fc35936fff
yosassenav <mromerovanessa.soy@gmail.com>
2025-01-26 18:59:54 -0500

    attempting to display list of fetched pokemons

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,23 +1,39 @@
 
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './slices/pokemon/thunks'
 
 export const PokemonApp = () => {
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  /**the useSelector function is responsible for
+   * selecting a part of the Redux's store state.
+   * NOTE: it receives the current state and returns a part of
+   * state or derived data.
+   */
+  const {loading, pokemons = [], page} = useSelector(state => state.pokemons);
 
   useEffect(()=>{
-    dispatch(getPokemons());
-  },[])
+    // calling the thunk: getPokemons
+    dispatch(getPokemons(0));
+  },[dispatch, page])
 
   return (
     <>
         <h1>Pokémon App</h1>
         <hr/>
+        <span>Loading...{loading ? 'true':'false'}</span>
         <ul>
-            <li></li>
+            {
+              pokemons.map((pokemon)=>{
+                return <li key={pokemon.name}>{pokemon.name}</li>
+              })
+            }
         </ul>
+        <button
+          disabled={loading}
+          onClick={() => dispatch(getPokemons(page))}
+        >Next</button>
     </>
   )
 }
